1a86cc442720295e63771b3e65582fccd3d4bfa2
Dennis James Stelmach <dj@dstelmach.com>
2024-04-02 08:41:05 -0400

    Formatting.

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,15 +1,25 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
+import partytown from "@astrojs/partytown";
+import tailwind from "@astrojs/tailwind";
+
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 import vue from "@astrojs/vue";
 import analogjsangular from "@analogjs/astro-angular";
 
-import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt(), react(), svelte(), vue(), analogjsangular(), partytown()]
+  integrations: [
+    tailwind(),
+    sitemap(),
+    robotsTxt(),
+    react(),
+    svelte(),
+    vue(),
+    analogjsangular(),
+    partytown(),
+  ],
 });
